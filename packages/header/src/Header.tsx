f205113659fef280152dce322a23f48f37509a9a
Jian Chuxin <chuxin.jian@shopee.com>
2025-07-08 17:48:40 +0800

    feat: update2

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export function Header() {
-  return <header>Lerna is the original monorepo tool! update1</header>;
+  return <header>Lerna is the original monorepo tool! update1 update2</header>;
 }
